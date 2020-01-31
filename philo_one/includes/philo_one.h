42ffb908a13c6a7be05376e0bcc37d7d7ff1f777
nendek <p.nardozi@gmail.com>
2020-01-31 22:57:54 +0100

    clean include

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -7,8 +7,6 @@
 # include <stdint.h>
 # include <pthread.h>
 
-#include <stdio.h>
-
 # include <sys/time.h>
 # include <sys/types.h>
 
@@ -25,7 +23,6 @@
 # define SLEEPING 2
 # define THINKING 3
 # define DEAD 4
-# define WAITING 5
 
 # define BUF_SIZE 4096
 
@@ -46,51 +43,50 @@ typedef struct			s_philo
 
 typedef struct			s_options
 {
-	uint16_t			number_of_philosopher;
-	time_t				time_to_die;
-	time_t				time_to_eat;
-	time_t				time_to_sleep;
-	time_t				number_of_time_each_philosophers_must_eat;
-}						t_options;
+	uint16_t		number_of_philosopher;
+	time_t			time_to_die;
+	time_t			time_to_eat;
+	time_t			time_to_sleep;
+	time_t			number_of_time_each_philosophers_must_eat;
+}				t_options;
 
 typedef struct			s_env
 {
-	t_options			options;
-	t_philo				*philos;
+	t_options		options;
+	t_philo			*philos;
 	pthread_mutex_t		*forks;
 	pthread_mutex_t		mutex_write;
 	pthread_mutex_t		mutex_free_fork;
 	pthread_t		monitor;
-	char				buf[BUF_SIZE];
-	int					buf_index;
+	char			buf[BUF_SIZE];
+	int			buf_index;
 
-	int					simulation_end;
-	time_t				time_end_simulation;
-}						t_env;
+	int			simulation_end;
+	time_t			time_end_simulation;
+}				t_env;
 
 /*			**** UTILS ****				*/
 int			ft_all_is_digit(char *s);
-long		ft_atol(const char *s);
-time_t		get_timestamp_ms(void);
+long			ft_atol(const char *s);
+time_t			get_timestamp_ms(void);
 int			print_message(t_env *env, int philo, uint8_t action);
-void		flush_buf(t_env *env);
+void			flush_buf(t_env *env);
 
 /*			**** INIT ****				*/
 int			init_options(int ac, char **av, t_options *options);
 int			init_env(t_env *env);
 
 /*			**** MAIN ****				*/
-void		*routine(void *p_data);
+void			*routine(void *p_data);
 
 /*			**** EAT ****				*/
 int			take_forks(t_philo *philo);
 int			free_forks(t_philo *philo);
 int			eat(t_philo *philo);
-void		snooze(t_philo *philo);
+void			snooze(t_philo *philo);
 int			think(t_philo *philo);
 
 /*			**** CLEAN ****				*/
 void			clean_env(t_env *env);
-
 
 #endif
